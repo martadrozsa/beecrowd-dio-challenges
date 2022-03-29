2d1f93e9d7c5af05d51551350ddfcfa324080c59
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2022-03-29 16:18:16 -0300

    Add new exercise

---
+++ src/TheonAnswer.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class TheonAnswer {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String strNumber = sc.nextLine();
+
+        String[] array = strNumber.split(" ");
+
+        int min = Integer.MAX_VALUE;
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+           int current = Integer.parseInt(array[i]);
+            if (current < min) {
+                min = current;
+                index = i;
+            }
+        }
+        int position = index + 1;
+        System.out.println(position);
+    }
+}
